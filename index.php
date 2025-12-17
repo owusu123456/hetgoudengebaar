e52092aca90a08249b69576314cf9efb57400e12
Andrew <andrewowusu@mac.home>
2025-12-17 14:49:49 +0100

    Header en intro toegevoegd

---
+++ index.php
@@ -0,0 +1,11 @@
+<header class="header">
+  <img src="images/logo.png" alt="Het Gouden Gebaar logo" class="logo">
+</header>
+
+<section class="intro">
+  <h1>Iedereen kan gebarentaal leren!</h1>
+  <p>
+    Cursussen Nederlandse Gebarentaal, workshops en trainingen op maat.<br>
+    Voor particulieren, scholen en bedrijven.
+  </p>
+</section>
